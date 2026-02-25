903a99c361f3d7d7a876c42ca8420a29517a8bd1
javiermirandamunoz01 <javiermirandamunoz01@gmail.com>
2026-02-25 17:00:57 +0000

    tratando de integrar un boton

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+let boton = document.getElementById("boton");
+let boton
+
+
+boton.addEventListener("click", function() {  
+    document.getElementById("arriba").scrollIntoView({
+        behavior:"smooth scroll"
+    })
+})
+
+
+
